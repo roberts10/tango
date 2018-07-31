d46da1f74b57ae5998b00334b1bd0b1fa5959142
roberts10 <scottrz@gmail.com>
2018-07-30 23:01:03 -0400

    modified:   rango/admin.py 	modified:   rango/forms.py 	modified:   rango/models.py 	modified:   rango/urls.py 	modified:   rango/views.py 	modified:   templates/rango/base.html 	new file:   templates/rango/cats.html 	new file:   templates/rango/login.html 	new file:   templates/rango/register.html

--- rango/admin.py
+++ rango/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Page)
+admin.site.register(UserProfile)
 
 #class CategoryAdmin(admin.ModelAdmin):
 #    prepopulated_fields = {'slug':('name',)}
