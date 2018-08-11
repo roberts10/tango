7042480c88e3eedcc2f3fbfeb3819a3383dd4e2c
roberts10 <scottrz@gmail.com>
2018-08-11 17:41:18 -0400

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   rango/urls.py
    	modified:   rango/views.py
    	modified:   tango_with_django_project/urls.py
    	modified:   templates/rango/add_category.html
    	modified:   templates/rango/add_page.html
    	modified:   templates/rango/base.html
    	new file:   templates/rango/base_bootstrap.html
    	modified:   templates/rango/category.html
    	modified:   templates/rango/index.html
    	modified:   templates/rango/login.html
    	copied:     templates/rango/base.html -> templates/rango/old_files/base.html
    	new file:   templates/rango/restricted.html
    	new file:   templates/registration/login.html
    	new file:   templates/registration/logout.html
    	new file:   templates/registration/registration_complete.html
    	new file:   templates/registration/registration_form.html
    
     Changes not staged for commit:
    	modified:   db.sqlite3
    	modified:   rango/__pycache__/urls.cpython-36.pyc
    	modified:   rango/__pycache__/views.cpython-36.pyc
    	modified:   tango_with_django_project/__pycache__/settings.cpython-36.pyc
    	modified:   tango_with_django_project/__pycache__/urls.cpython-36.pyc
    	modified:   tango_with_django_project/settings.py
    	deleted:    test.txt
    
     Untracked files:
    	rango/webhose_search.py
    	static/
    	tango_with_django_project/search.key
    	user_pass

--- rango/views.py
+++ rango/views.py
@@ -2,21 +2,51 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rango.models import Category, Page, UserProfile
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
-
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
 import os
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
+
+
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages':page_list}
-    return render(request, 'rango/index.html', context_dict)
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
     print(request.method)
     print(request.user)
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED")
+        request.session.delete_test_cookie()
     context_dict = {}
     return render(request, 'rango/about.html', context=context_dict)
 
@@ -32,6 +62,7 @@ def show_category(request, category_name_slug):
         context_dict['pages']=None
     return render(request, 'rango/category.html', context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -43,8 +74,10 @@ def add_category(request):
             return index(request) 
         else:
             print(form.errors)
+            print("ERROR")
     return render(request, 'rango/add_category.html', {'form':form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -67,46 +100,56 @@ def add_page(request, category_name_slug):
     context_dict = {'form':form, 'category': category}
     return render(request, 'rango/add_page.html', context_dict)
 
-def register(request):
-        registered = False 
-        if request.method == 'POST':
-            user_form  = UserForm(data=request.POST)
-            profile_form = UserProfileForm(data=request.POST)
-            if user_form.is_valid() and profile_form.is_valid():
-                user = user_form.save()
-                user.set_password(user.password)
-                user.save()
-
-                profile = profile_form.save(commit=False)
-                profile.user = user
-                if 'picture' in request.FILES:
-                    profile.picture = request.FILES['picture']
-
-                profile.save()
-            else:
-                print(user_form.errors, profile_form.errors)
-        else:
-            user_form = UserForm()
-            profile_form = UserProfileForm()
-
-        return render(request, 'rango/register.html', {'user_form' : user_form, 'profile_form': profile_form, 'registered': registered})
-
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(username=username, password=password)
-        
-        if user:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-            else:
-                return HttpResponse("Your Rango account is disabled")
-        else:
-            print("Invalid login details: {0}, {1}".format(username, password))
-            return HttpResponse("Invalid login details supplied.")
-    else:
-        return render(request, 'rango/login.html', {})
+#def register(request):
+#        registered = False 
+#        if request.method == 'POST':
+#            user_form  = UserForm(data=request.POST)
+#            profile_form = UserProfileForm(data=request.POST)
+#            if user_form.is_valid() and profile_form.is_valid():
+#                user = user_form.save()
+#                user.set_password(user.password)
+#                user.save()
+#
+#                profile = profile_form.save(commit=False)
+#                profile.user = user
+#                if 'picture' in request.FILES:
+#                    profile.picture = request.FILES['picture']
+#
+#                profile.save()
+#            else:
+#                print(user_form.errors, profile_form.errors)
+#        else:
+#            user_form = UserForm()
+#            profile_form = UserProfileForm()
+#
+#        return render(request, 'rango/register.html', {'user_form' : user_form, 'profile_form': profile_form, 'registered': registered})
+#
+#def user_login(request):
+#    if request.method == 'POST':
+#        username = request.POST.get('username')
+#        password = request.POST.get('password')
+#
+#        user = authenticate(username=username, password=password)
+#        
+#        if user:
+#            if user.is_active:
+#                login(request, user)
+#                return HttpResponseRedirect(reverse('index'))
+#            else:
+#                return HttpResponse("Your Rango account is disabled")
+#        else:
+#            print("Invalid login details: {0}, {1}".format(username, password))
+#            return HttpResponse("Invalid login details supplied.")
+#    else:
+#        return render(request, 'rango/login.html', {})
+#
+#@login_required
+#def restricted(request):
+#    return render(request, 'rango/restricted.html', {})
+#
+#@login_required
+#def user_logout(request):
+#    logout(request)
+#    return HttpResponseRedirect(reverse('index'))
+#
 
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,11 +17,17 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
+from registration.backends.simple.views import RegistrationView
 
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^about/', views.about, name ='about'),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
