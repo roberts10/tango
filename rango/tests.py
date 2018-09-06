995545489d61adc66de4cf39476f4ecd457e9d5c
roberts10 <scottrz@gmail.com>
2018-09-06 13:11:53 -0400

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   rango/tests.py
    
     Changes not staged for commit:
    	modified:   db.sqlite3
    	modified:   rango/__pycache__/forms.cpython-36.pyc
    	modified:   rango/__pycache__/urls.cpython-36.pyc
    	modified:   rango/__pycache__/views.cpython-36.pyc
    	modified:   tango_with_django_project/__pycache__/settings.cpython-36.pyc
    	modified:   tango_with_django_project/__pycache__/urls.cpython-36.pyc
    	deleted:    test.txt
    
     Untracked files:
    	,
    	media/
    	rango/.models.py.swp
    	rango/.tests.py.swp
    	rango/.urls.py.swp
    	rango/.views.py.swp
    	rango/__pycache__/tests.cpython-36.pyc
    	rango/__pycache__/webhose_search.cpython-36.pyc
    	search.key
    	static/
    	templates/rango/.base.html.swp
    	templates/rango/.category.html.swp
    	templates/rango/.cats.html.swp
    	user_pass

--- rango/tests.py
+++ rango/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rango.models import Category
+from django.core.urlresolvers import reverse
+
+class CategoryMethodTests(TestCase):
+    def test_ensure_views_are_positive(self):
+        cat = Category(name='test', views=1, likes=0)
+        cat.save()
+        self.assertEqual((cat.views >= 0), True)
+
+    def test_slug_line_creation(self):
+        cat = Category(name='Random Category String', views=-1, likes=0)
+        cat.save()
+        self.assertEqual(cat.slug, 'random-category-string')
+
+class IndexViewTest(TestCase):
+    def test_index_view_with_no_categories(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no categories present.")
+        self.assertQuerysetEqual(response.context['categories'], [])
+
+    def add_cat(name, views, likes):
+        c = Category.objects.get_or_create(name=name)[0]
+        c.views = views
+        c.likes = likes
+        c.save()
+        return c
+
+    def test_index_view_with_categories(self):
+        add_cat('test',1,1,)
+        add_cat('temp',1,1,)
+        add_cat('tmp',1,1,)
+        add_cat('tmp test temp',1,1,)
+
+        reponse = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "tmp test temp")
+
+        num_cats = len(response.context['categories'])
+        self.assertEqual(num_cats, 4)
+
+
+
+
+
