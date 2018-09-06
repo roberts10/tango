3ba619347a78534f77bd358f05ad55846ac9c9fb
roberts10 <scottrz@gmail.com>
2018-09-06 15:07:58 -0400

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	deleted:    db.sqlite3
    	new file:   static/images/rango.jpg
    	new file:   static/js/rango-ajax.js
    
     Changes not staged for commit:
    	modified:   rango/__pycache__/forms.cpython-36.pyc
    	modified:   rango/__pycache__/urls.cpython-36.pyc
    	modified:   rango/__pycache__/views.cpython-36.pyc
    	modified:   tango_with_django_project/__pycache__/settings.cpython-36.pyc
    	modified:   tango_with_django_project/__pycache__/urls.cpython-36.pyc
    	deleted:    test.txt
    
     Untracked files:
    	,
    	db.sqlite3
    	media/
    	rango/.models.py.swp
    	rango/.tests.py.swp
    	rango/.urls.py.swp
    	rango/.views.py.swp
    	rango/__pycache__/tests.cpython-36.pyc
    	rango/__pycache__/webhose_search.cpython-36.pyc
    	search.key
    	templates/rango/.base.html.swp
    	templates/rango/.category.html.swp
    	templates/rango/.cats.html.swp
    	user_pass

---
+++ static/js/rango-ajax.js
@@ -0,0 +1,53 @@
+ $(document).ready(function (){
+
+    $("#about-btn").click(function(event){
+        alert("you clicked with Jquery");
+    });
+
+    $("p").hover(function(){
+        $(this).css('color', 'red');
+    },
+        function(){
+            $(this).css('color', 'blue');
+        });
+
+    $("#about-btn").click(function(event){
+        msgstr = $("#msg").html()
+        msgstr = msgstr + "ooo"
+        $("#msg").html(msgstr)
+    });
+
+
+    $('#likes').click(function(){
+        var catid;
+        catid = $(this).attr("data-catid");
+        $.get('/rango/like/', {category_id:catid}, function(data){
+            $('#like_count').html(data);
+                $('#likes').hide();
+        });
+    });
+
+    $('#suggestion').keyup(function(){
+        var query;
+        query = $(this).val();
+
+        $.get('/rango/suggest/', {suggestion: query}, function(data){
+            $('#cats').html(data);
+        });
+    });
+
+     $('.rango-add').click(function(){
+        alert('click recorded on rango-add button')
+        var catid = $(this).attr("data-catid");
+        var url = $(this).attr("data-url");
+        var title = $(this).attr("data-title");
+        var me = $(this);
+
+        $.get('/rango/add/', {category_id: catid, url: url, title: title}, function(data){
+            $('#pages').html(data);
+            me.hide();
+        });
+    });
+
+
+});
